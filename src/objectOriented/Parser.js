f1a82a04a650546e4476c5e0152049a513733ab2
Andrey <losslessring@yandex.ru>
2023-12-22 01:33:21 +0300

    Parsing numeric literal from string

---
+++ src/objectOriented/Parser.js
@@ -0,0 +1,21 @@
+class Parser {
+    parse(string) {
+        this._string = string
+        return this.Program()
+    }
+
+    Program() {
+        return this.NumericLiteral()
+    }
+
+    NumericLiteral() {
+        return {
+            type: "NumericLiteral",
+            value: Number(this._string),
+        }
+    }
+}
+
+module.exports = {
+    Parser,
+}
---
+++ src/tests/objectOriented/numericLiteral/numericLiteral.test.js
@@ -0,0 +1,29 @@
+const { Parser } = require("../../../objectOriented/Parser")
+
+describe("parse number", () => {
+    it("returns numeric literal from string '42'", () => {
+        const parser = new Parser()
+
+        const program = `42`
+        const ast = parser.parse(program)
+
+        const result = {
+            type: "NumericLiteral",
+            value: 42,
+        }
+        expect(ast).toStrictEqual(result)
+    })
+
+    it("returns numeric literal from string '111'", () => {
+        const parser = new Parser()
+
+        const program = `111`
+        const ast = parser.parse(program)
+
+        const result = {
+            type: "NumericLiteral",
+            value: 111,
+        }
+        expect(ast).toStrictEqual(result)
+    })
+})
